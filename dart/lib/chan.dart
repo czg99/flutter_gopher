582fd891ad4d18129e1a214d675c9f42e6643a7e
czg99 <chenzhigao99@gmail.com>
2025-05-31 02:28:44 +0800

    docs: 为外部使用到的异常处理和通道类添加英文注释

--- dart/lib/chan.dart
+++ dart/lib/chan.dart
@@ -4,23 +4,29 @@ import 'dart:isolate';
 import 'dart:ui';
 
 /// 用于在Go到Dart之间传递数据的通道
+/// Channel for passing data between Go and Dart
 ///
-/// FgChan是一个泛型类，用于从Go代码接收数据并转换为Dart对象。
-/// 它使用Dart的[ReceivePort]和[IsolateNameServer]来实现跨语言通信。
+/// FgChan是一个泛型类，用于从Go代码接收数据并转换为Dart对象
+/// 使用Dart的[ReceivePort]和[IsolateNameServer]来实现跨语言通信
 class FgChan<T> {
   /// 接收端口，用于接收来自Go的消息
+  /// The receive port for receiving messages from Go
   ReceivePort? _receivePort;
 
   /// 指针到Dart对象的转换函数
+  /// The pointer-to-Dart object conversion function
   T Function(Pointer<Void>)? _pointerToDart;
 
   /// 端口ID，用于在IsolateNameServer中注册
+  /// The port ID for registering in IsolateNameServer
   int? _portId;
 
   /// 获取当前通道的端口ID
+  /// Get the current channel port ID
   int get portId => _portId ?? 0;
 
   /// 获取Dart对象数据流
+  /// Get the Dart object data stream
   Stream<T> get stream {
     if (_receivePort == null) {
       throw StateError('Channel is closed');
@@ -36,12 +42,15 @@ class FgChan<T> {
   }
 
   /// 检查通道是否已关闭
+  /// Check if the channel is closed
   bool get isClosed => _receivePort == null;
 
   /// 创建一个通道
+  /// Create a channel
   FgChan() : _receivePort = ReceivePort();
 
   /// 监听Dart对象数据流
+  /// Listen to the Dart object data stream
   StreamSubscription listen(
     void Function(T) onData, {
     Function? onError,
@@ -57,6 +66,7 @@ class FgChan<T> {
   }
 
   /// 设置端口ID并注册到IsolateNameServer，不要手动调用
+  /// Set the port ID and register to IsolateNameServer, do not call manually
   void setPortId(int portId) {
     if (_receivePort == null) {
       throw StateError('Channel is closed');
@@ -80,11 +90,13 @@ class FgChan<T> {
   }
 
   /// 设置指针到Dart对象的转换函数，不要手动调用
+  /// Set the pointer-to-Dart object conversion function, do not call manually
   void setPointerToDart(T Function(Pointer<Void>) converter) {
     _pointerToDart = converter;
   }
 
   /// 关闭Dart通道并清理资源
+  /// Close the Dart channel and clean up resources
   void close() {
     if (_portId != null) {
       IsolateNameServer.removePortNameMapping(_portId.toString());
--- dart/lib/exception.dart
+++ dart/lib/exception.dart
@@ -1,16 +1,20 @@
-/// 提供统一的异常处理机制，包括可恢复的错误和不可恢复的严重错误
 /// 所有 Flutter Gopher 异常的基类
+/// All Flutter Gopher exceptions base class
 abstract class FgException implements Exception {
   /// 异常发生的库名称
+  /// The library name where the exception occurred
   final String libName;
 
   /// 异常发生的函数名称
+  /// The function name where the exception occurred
   final String funcName;
 
   /// 异常的描述信息
+  /// The description of the exception
   final String message;
 
   /// 创建一个包含指定上下文信息的 FgException
+  /// Creates a FgException with the specified context information
   const FgException(this.libName, this.funcName, this.message);
 
   @override
@@ -18,8 +22,10 @@ abstract class FgException implements Exception {
 }
 
 /// 表示库中的可恢复错误
+/// Represents a recoverable error in the library
 final class FgError extends FgException {
   /// 创建一个包含指定上下文信息的可恢复错误
+  /// Creates a recoverable error with the specified context information
   const FgError(super.libName, super.funcName, super.message);
 
   @override
@@ -30,6 +36,7 @@ final class FgError extends FgException {
 /// 表示库中的严重且不可恢复的错误
 final class FgPanic extends FgException {
   /// 创建一个包含指定上下文信息的不可恢复错误
+  /// Creates a unrecoverable error with the specified context information
   const FgPanic(super.libName, super.funcName, super.message);
 
   @override
