689b90ab7cbee9a06a74b17c8dff57905c9fbdca
czg99 <chenzhigao99@gmail.com>
2025-05-31 02:05:42 +0800

    feat(plugin templates): 添加通道示例，依赖库要求0.1.0

--- plugin_gen/templates/src/api/sum.go
+++ plugin_gen/templates/src/api/sum.go
@@ -1,5 +1,18 @@
 package api
 
-func Sum(a, b int) int {
+import "time"
+
+func Add(a, b int) int {
 	return a + b
+}
+
+func Tick() chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			c <- 1
+		}
+	}()
+	return c
 }
