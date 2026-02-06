a886356723c1ec748dff23b76d80934211221e1b
czg99 <chenzhigao99@gmail.com>
2026-02-06 18:04:25 +0800

    fix: 修复bridge.h模板中缺失的分号

--- plugin_gen/templates/gosrc/bridge/include/bridge.h
+++ plugin_gen/templates/gosrc/bridge/include/bridge.h
@@ -44,13 +44,13 @@ extern DLLEXPORT void fg_call_platform_method_async_{{.ID}}(int64_t port, FgRequ
 
 static inline void fg_bridge_binding_{{.ID}}(void) {
     uintptr_t ptr = 0;
-	ptr ^= (uintptr_t)fg_init_dart_api_{{.ID}}
-	ptr ^= (uintptr_t)fg_init_platform_method_handle_{{.ID}}
-	ptr ^= (uintptr_t)fg_call_dart_method_{{.ID}}
-	ptr ^= (uintptr_t)fg_call_go_method_{{.ID}}
-	ptr ^= (uintptr_t)fg_call_go_method_async_{{.ID}}
-	ptr ^= (uintptr_t)fg_call_platform_method_{{.ID}}
-	ptr ^= (uintptr_t)fg_call_platform_method_async_{{.ID}}
+	ptr ^= (uintptr_t)fg_init_dart_api_{{.ID}};
+	ptr ^= (uintptr_t)fg_init_platform_method_handle_{{.ID}};
+	ptr ^= (uintptr_t)fg_call_dart_method_{{.ID}};
+	ptr ^= (uintptr_t)fg_call_go_method_{{.ID}};
+	ptr ^= (uintptr_t)fg_call_go_method_async_{{.ID}};
+	ptr ^= (uintptr_t)fg_call_platform_method_{{.ID}};
+	ptr ^= (uintptr_t)fg_call_platform_method_async_{{.ID}};
 }
 
 #endif
