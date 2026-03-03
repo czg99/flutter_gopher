45276048dd6f96d0359816b3791b676e72add7ef
czg99 <chenzhigao99@gmail.com>
2026-03-03 17:17:02 +0800

    feat: ohos新增异步调用平台代码功能

--- plugin_gen/templates/gosrc/bridge/cpp/napi_init.cpp
+++ plugin_gen/templates/gosrc/bridge/cpp/napi_init.cpp
@@ -1,8 +1,13 @@
 #include <napi/native_api.h>
+#include <pthread.h>
 #include "../include/bridge.h"
 
+static void callJsCallback(napi_env env, napi_value js_cb, void* context, void* data);
+
+static napi_threadsafe_function g_tsfn = NULL;
 static napi_ref g_platformCallbackRef = NULL;
 static napi_env g_env = NULL;
+static pthread_t g_main_thread_id = 0;
 
 static void free_fgdata(FgData *data) {
     if (data->data != NULL) {
@@ -76,7 +81,15 @@ static napi_value fgresponse_to_js(napi_env env, FgResponse resp) {
     return result;
 }
 
+static int is_main_thread(void) {
+    return pthread_equal(pthread_self(), g_main_thread_id);
+}
+
 static napi_value napi_init_platform_method_handle(napi_env env, napi_callback_info info) {
+    g_main_thread_id = pthread_self();
+
+    g_env = env;
+
     size_t argc = 1;
     napi_value args[1] = {NULL};
 
@@ -85,26 +98,42 @@ static napi_value napi_init_platform_method_handle(napi_env env, napi_callback_i
         return NULL;
     }
 
-    napi_value value = args[0];
+    napi_value callback = args[0];
 
-    if (value != NULL) {
-        napi_valuetype valueType = napi_undefined;
-        napi_typeof(env, value, &valueType);
-        if (valueType != napi_function) {
-            return NULL;
-        }
+    napi_valuetype valueType = napi_undefined;
+    napi_typeof(env, callback, &valueType);
+    if (valueType != napi_function) {
+        return NULL;
     }
+
+    if (g_tsfn != NULL) {
+        napi_release_threadsafe_function(g_tsfn, napi_tsfn_abort);
+        g_tsfn = NULL;
+    }
+
+    napi_value async_resource_name = NULL;
+    napi_create_string_utf8(env, "{{.LibName}}", NAPI_AUTO_LENGTH, &async_resource_name);
+
+    napi_create_threadsafe_function(
+        env,
+        callback,
+        NULL,
+        async_resource_name,
+        8,
+        1,
+        NULL,
+        NULL,
+        NULL,
+        callJsCallback,
+        &g_tsfn
+    );
 
     if (g_platformCallbackRef != NULL) {
         napi_delete_reference(env, g_platformCallbackRef);
         g_platformCallbackRef = NULL;
     }
 
-    if (value != NULL) {
-        napi_create_reference(env, value, 1, &g_platformCallbackRef);
-    }
-    
-    g_env = env;
+    napi_create_reference(env, callback, 1, &g_platformCallbackRef);
     return NULL;
 }
 
@@ -155,6 +184,14 @@ __attribute__((constructor)) static void RegisterModule(void)
     napi_module_register(&{{.LibName}}Module);
 }
 
+typedef struct {
+    FgRequest req;
+    FgResponse resp;
+    bool done;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} FgThreadObject;
+
 __attribute__((noinline)) static FgResponse fg_call_platform_method(FgRequest request) {
     FgResponse resp = {};
     if (g_platformCallbackRef == NULL || g_env == NULL) {
@@ -193,4 +230,40 @@ __attribute__((noinline)) static FgResponse fg_call_platform_method(FgRequest re
 
     napi_close_handle_scope(g_env, scope);
     return resp;
+}
+
+__attribute__((noinline)) static FgResponse fg_call_platform_method_safe(FgRequest request) {
+    if (is_main_thread()) {
+        return fg_call_platform_method(request);
+    }
+
+    if (g_tsfn == NULL) {
+        return (FgResponse){};
+    }
+
+    FgThreadObject obj = {};
+    obj.req = request;
+    obj.resp = (FgResponse){};
+    obj.done = false;
+    pthread_mutex_init(&obj.mutex, NULL);
+    pthread_cond_init(&obj.cond, NULL);
+
+    napi_call_threadsafe_function(g_tsfn, &obj, napi_tsfn_blocking);
+
+    pthread_mutex_lock(&obj.mutex);
+    while (!obj.done) {
+        pthread_cond_wait(&obj.cond, &obj.mutex);
+    }
+    pthread_mutex_unlock(&obj.mutex);
+    return obj.resp;
+}
+
+static void callJsCallback(napi_env env, napi_value js_cb, void* context, void* data) {
+    FgThreadObject *obj = (FgThreadObject *)data;
+    obj->resp = fg_call_platform_method(obj->req);
+
+    pthread_mutex_lock(&obj->mutex);
+    obj->done = true;
+    pthread_cond_signal(&obj->cond);
+    pthread_mutex_unlock(&obj->mutex);
 }
